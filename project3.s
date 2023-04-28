d9c44091b557f0b1f5cdee966d3be717962353c6
Ivaneisha02 <90430746+Ivaneisha02@users.noreply.github.com>
2023-04-28 09:06:15 -0400

    added delimiter, () , error message and input

--- project3.s
+++ project3.s
@@ -7,23 +7,4 @@ left_paren: .asciiz "("
 right_paren: .asciiz ")"
 
 .text
-main:
-    li $v0, 8
-    la $a0, input
-    li $a1, 1000
-    syscall
-    addi $sp, $sp, -4
-    la $t0, input
-    sw $t0, ($sp)
-    jal sub_a
-    addi $sp, $sp,4
-exit: 
-    li $v0, 10
-    li $a0, 0
-    syscall
-sub_a:
-    addi $sp, $sp, -12
-    sw $ra, ($sp)
-    sw $s0, 4($sp)
-    sw $s1, 8($sp)
-    lw $s0, 12($sp)
+
